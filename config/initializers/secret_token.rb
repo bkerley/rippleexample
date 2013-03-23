6ca20447df5399b57642a24c37b5c96333bf794b
Bryce Kerley <bkerley@brycekerley.net>
2013-03-23 13:46:42 -0400

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rippleexample::Application.config.secret_token = 'c5250f59bdee05a375c6c19293d57e46d8750544f8489ed3e89d6e3126f6eb07129ab57f9b53e385da9aaf997a7a949674403b91a2944294e7f4159879831be4'
